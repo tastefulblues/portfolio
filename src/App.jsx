de1717bc674fc10991160cefc8209e78f69b8486
tastefulblues <maxjh28@gmail.com>
2026-03-16 12:45:17 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+  <>
+    <div className = "container">
+      <div className = "name">
+        <h3>Max Hunt</h3>
+        <p><a href = "mailto:hi@maxjh.dev">hi@maxjh.dev</a></p>
+      </div>
+      <a href = "https://github.com/tastefulblues">
+      <img src = "/src/assets/GitHub_Invertocat_White.png" class = "git"></img>
+      </a>
+
+    </div>
+    <hr></hr>
+    <div className = "bio">
+      <p>
+        Currently a student at the University of Utah,
+        based in Salt Lake City. Thanks for stopping by.
+      </p>
+    </div>
+    <hr></hr>
+    <div className = "bio">
+      Find some info below.
+    </div>
+  </>  
+  )
+}
+export default App
